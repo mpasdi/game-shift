648abb566574a9149fc2fc8747378319f8f2bf5b
LSM <2779355293@qq.com>
2026-07-30 14:22:29 +0800

    feat(app): 🚀 接入Tauri Updater 插件

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -33,6 +33,7 @@ fn app_info(app: tauri::AppHandle) -> Result<AppInfo, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .setup(|app| {
             db::initialize(app.handle())?;
